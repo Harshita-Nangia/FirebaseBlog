8090ebd429325f6e2059cdaea5da0720302306ab
Harshita-Nangia <hn.nangia98@gmail.com>
2021-08-24 13:12:13 +0530

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,61 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDNNCtxCzfqRICTO3bThUNoLPPQr3UgAjs",
+    authDomain: "myblog-dec51.firebaseapp.com",
+    databaseURL: "https://myblog-dec51-default-rtdb.firebaseio.com",
+    projectId: "myblog-dec51",
+    storageBucket: "myblog-dec51.appspot.com",
+    messagingSenderId: "317136384334",
+    appId: "1:317136384334:web:f6744db489d48938f96e1f",
+    measurementId: "G-BNXT8BH29D"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  let postCollection = document.querySelector("#posts-collection");
+
+  const db = firebase.firestore();
+  
+  function createPost(title,creator, time, content) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "col-md-3");
+  
+    let h2 = document.createElement("h2");
+    let h5 = document.createElement("h5");
+    let p = document.createElement("p");
+    let small = document.createElement("small");
+  
+    h2.textContent = title;
+    h5.textContent= creator;
+    small.textContent = time;
+    p.textContent = content;
+  
+    div.appendChild(h2);
+    div.appendChild(h5);
+    div.appendChild(small);
+    div.appendChild(p);
+  
+    postCollection.appendChild(div);
+  }
+  
+  // Get Posts
+  function getPosts() {
+    db.collection("posts")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(docs => {
+          createPost(
+            docs.data().postName,
+            docs.data().author,
+            docs.data().createdAt,
+            docs.data().postContent
+          );
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  
+  getPosts();
---
+++ public/create.js
@@ -0,0 +1,22 @@
+document.querySelector("#submitBtn").addEventListener("click", function() {
+    let postAuthor = document.querySelector("#author").value;
+    let postTitle = document.querySelector("#postTitle").value;
+    let postContent = document.querySelector("#postContent").value;
+    let postDate = document.querySelector("#postDate").value;
+  
+    if (
+      postAuthor === "" ||
+      postTitle === "" ||
+      postContent === "" ||
+      postDate === ""
+    ) {
+      alert("Required Field cannot be empty");
+    } else {
+      db.collection("posts").doc().set({
+          author: postAuthor,
+          createdAt: postDate,
+          postName: postTitle,
+          postContent: postContent
+        });
+    }
+  });
